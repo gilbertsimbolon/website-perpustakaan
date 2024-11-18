a2597e130a9997ddb45de08482e8944b784f08a6
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-18 17:49:48 +0800

    konfigurasi model dan migrasi library information

--- database/migrations/2024_11_18_094418_create_library_information_table.php
+++ database/migrations/2024_11_18_094418_create_library_information_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('library_information', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('address');
             $table->timestamps();
         });
     }
