b2a34dcfe344261ae157ae87508f5489286b3fd5
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-18 18:04:01 +0800

    konfigurasi LibraryInformationController

--- app/Http/Controllers/LibraryInformationController.php
+++ app/Http/Controllers/LibraryInformationController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LibraryInformation;
 use Illuminate\Http\Request;
 
 class LibraryInformationController extends Controller
 {
-    //
+    public function index()
+    {
+    $library = LibraryInformation::first();
+    return view('library.index', compact('library'));
+    }
 }
