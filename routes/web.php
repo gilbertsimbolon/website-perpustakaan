3709094812b3780f7fd9dfd4ad077c86f5785ed2
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-18 19:04:24 +0800

    membuat blade engine

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\LibraryInformationController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('home');
+});
 
-Route::get('/', [LibraryInformationController::class, 'index'])->name('home');
+Route::get('/visi-misi', function() {
+    return view('visi');
+});
