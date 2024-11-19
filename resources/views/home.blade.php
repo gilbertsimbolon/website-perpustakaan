3f7c87f25801c90675f509230875f966b9c17603
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-19 11:06:49 +0800

    membuat component header dan navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,78 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <title>Home</title>
 </head>
 <body>
     <div class="bg-white">
-  <header class="absolute inset-x-0 top-0 z-50">
-    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
-          <span class="sr-only">Perpustakaan</span>
-          <img class="h-8 w-auto" src="https://unima.ac.id/uploads/img_logo/1650416196421.png" alt="">
-        </a>
-      </div>
-      <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-          <span class="sr-only">Open main menu</span>
-          <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-        </button>
-      </div>
-      <div class="hidden lg:flex lg:gap-x-12">
-        <a href="/" class="text-sm/6 font-semibold text-gray-900">Home</a>
-        <a href="visi-misi" class="text-sm/6 font-semibold text-gray-900">Visi Misi</a>
-        <a href="struktur" class="text-sm/6 font-semibold text-gray-900">Struktur</a>
-        <a href="pegawai" class="text-sm/6 font-semibold text-gray-900">Pegawai</a>
-        <a href="buku" class="text-sm/6 font-semibold text-gray-900">Buku</a>
-        <a href="galeri" class="text-sm/6 font-semibold text-gray-900">Galeri</a>
-        <a href="video" class="text-sm/6 font-semibold text-gray-900">Video</a>
-        <a href="kontak" class="text-sm/6 font-semibold text-gray-900">Kontak</a>
-      </div>
-      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        {{-- Comment : Fitur Login di Matikan --}}
-        {{-- <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a> --}}
-      </div>
-    </nav>
-    <!-- Mobile menu, show/hide based on menu open state. -->
-    <div class="lg:hidden" role="dialog" aria-modal="true">
-      <!-- Background backdrop, show/hide based on slide-over state. -->
-      <div class="fixed inset-0 z-50"></div>
-      <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-        <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Perpustakaan</span>
-            <img class="h-8 w-auto" src="https://unima.ac.id/uploads/img_logo/1650416196421.png" alt="">
-          </a>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-            <span class="sr-only">Close menu</span>
-            <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/10">
-            <div class="space-y-2 py-6">
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Home</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Visi Misi</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Struktur</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Pegawai</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Buku</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Galeri</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Video</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Kontak</a>
-            </div>
-            <div class="py-6">
-              {{-- <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a> --}}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
+    <x-header>
+      
+    </x-header>
+
+    <x-navbar></x-navbar>
 
     <section id="home" class="relative isolate">
         <div class="relative isolate px-6 pt-14 lg:px-8">
