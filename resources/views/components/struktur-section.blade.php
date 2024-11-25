deb8f217ff370a6c2cda0ab84aa7f408a1176d98
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2024-11-25 17:45:23 +0800

    memperbaiki section struktur

--- resources/views/components/struktur-section.blade.php
+++ resources/views/components/struktur-section.blade.php
@@ -1,30 +1,5 @@
-<section id="struktur" class="flex flex-col justify-start items-center pt-14 h-[calc(100vh-64px)] text-center text-white px-6">
-    <div class="max-w-7xl mx-auto text-center"">
-        <h2 class="text-xl font-bold font-serif text-gray-900">
-            STRUKTUR ORGANISASI<br>UNIT PENUNJANG AKADEMIK PERPUSTAKAAN<br>UNIVERSITAS NEGERI MANADO
-        </h2>
-    </div>
-    <div id="tree" class="mt-5 p-4 rounded-lg">
-        <ul class="organization-tree">
-            <li>
-                <div class="node">Direktur Utama</div>
-                <ul>
-                    <li>
-                        <div class="node">Manajer Keuangan</div>
-                        <ul>
-                            <li><div class="node">Staf Keuangan 1</div></li>
-                            <li><div class="node">Staf Keuangan 2</div></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <div class="node">Manajer Operasional</div>
-                        <ul>
-                            <li><div class="node">Staf Operasional 1</div></li>
-                            <li><div class="node">Staf Operasional 2</div></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+<section id="struktur" class="flex flex-col justify-start items-center h-[calc(100vh-64px)] text-center text-white px-6">
+    <div class="w-full h-full mx-auto text-center">
+        <img src="{{ asset('img/struktur.jpg') }}" alt="Struktur Organisasi" class="w-full h-full object-contain">
     </div>
 </section>
